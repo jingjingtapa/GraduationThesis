642396bf2d0cbfc6d984ff03332ed7a384d5e236
jingjingtapa <jingjingtapa@naver.com>
2024-10-04 15:28:33 +0900

    BEV image without motion cancel

--- BEV/backward.py
+++ BEV/backward.py
@@ -1,3 +1,5 @@
+from cityscapesscripts.helpers.box3dImageTransform import Camera,Box3dImageTransform, CRS_V, CRS_C, CRS_S
+from cityscapesscripts.helpers.annotation import CsBbox3d
 import cv2, json, os, math
 import numpy as np
 
@@ -26,27 +28,6 @@ def rotation_from_euler(roll=1., pitch=1., yaw=1.):
     R[2, 2] = cj * ci
     return R
 
-def euler_from_rotation(R):
-    assert(R.shape == (3, 3))
-
-    if R[2, 0] != 1 and R[2, 0] != -1:
-        pitch1 = -np.arcsin(R[2, 0])
-        pitch2 = np.pi - pitch1
-        roll1 = np.arctan2(R[2, 1] / np.cos(pitch1), R[2, 2] / np.cos(pitch1))
-        roll2 = np.arctan2(R[2, 1] / np.cos(pitch2), R[2, 2] / np.cos(pitch2))
-        yaw1 = np.arctan2(R[1, 0] / np.cos(pitch1), R[0, 0] / np.cos(pitch1))
-        yaw2 = np.arctan2(R[1, 0] / np.cos(pitch2), R[0, 0] / np.cos(pitch2))
-        return (roll1, pitch1, yaw1), (roll2, pitch2, yaw2)
-    else:
-        yaw = 0  # can set to anything, it's the gimbal lock case
-        if R[2, 0] == -1:
-            pitch = np.pi / 2
-            roll = yaw + np.arctan2(R[0, 1], R[0, 2])
-        else:
-            pitch = -np.pi / 2
-            roll = -yaw + np.arctan2(-R[0, 1], -R[0, 2])
-        return (roll, pitch, yaw), (None, None, None)
-
 def translation_matrix(vector):
     # Translation vector -> T [4, 4]
     M = np.identity(4)
@@ -55,31 +36,13 @@ def translation_matrix(vector):
 
 def motion_cancel(cal):
     imu = np.array(cal['sensor']['sensor_T_ISO_8855']) # 3*4
-    rotation, translation = imu[:,:3], imu[:2,2]
+    rotation, translation = imu[:2,:2].T, -imu[:2,3]
     
-    euler_angle_set_1, euler_angle_set_2 = euler_from_rotation(rotation)
-    
-    roll, pitch, yaw = euler_angle_set_1
-    
-    R_roll = np.array([
-        [np.cos(roll), -np.sin(roll)],
-        [np.sin(roll), np.cos(roll)]
-    ])
-    
-    R_pitch = np.array([
-        [np.cos(pitch), -np.sin(pitch)],
-        [np.sin(pitch), np.cos(pitch)]
-    ])
-    
-    R_2d = R_roll @ R_pitch
+    motion_cancel_mat = np.identity(3)
+    motion_cancel_mat[:2,:2] = rotation
+    motion_cancel_mat[:2,2] = translation
 
-    rotation_inv, translation_inv = R_pitch.T, -translation
-
-    imu_inv = np.identity(3)
-    imu_inv[:2,:2] = rotation_inv
-    imu_inv[:2,2] = translation_inv
-    
-    return imu_inv
+    return motion_cancel_mat
 
 def load_camera_params(cal): #c: calibration
     fx, fy = cal['intrinsic']['fx'], cal['intrinsic']['fy']
@@ -97,7 +60,7 @@ def load_camera_params(cal): #c: calibration
 
     # Extrinsic
     R_veh2cam = np.transpose(rotation_from_euler(roll, pitch, yaw))
-    T_veh2cam = translation_matrix((-x+baseline/2, -y, -z))
+    T_veh2cam = translation_matrix((-x-baseline/2, -y, -z))
 
     # Rotate to camera coordinates
     R = np.array([[0., -1., 0., 0.],
@@ -125,80 +88,22 @@ def generate_direct_backward_mapping(
     world_z = 0
     for i, world_x in enumerate(world_x_coords):
         for j, world_y in enumerate(world_y_coords):
-            # world_coord : [world_x, world_y, 0, 1]
-            # uv_coord : [u, v, 1]
-            
+            # [world_x, world_y, 0, 1] -> [u, v, 1]
             world_coord = [world_x, world_y, world_z, 1]
             camera_coord = extrinsic[:3, :] @ world_coord # 3*4 * 4*1 = 3*1
             uv_coord = intrinsic[:3, :3] @ camera_coord # 3*3 * 3*1 = 3*1
             uv_coord /= uv_coord[2]
 
-            uv_coord = motion_cancel_mat @ uv_coord
+            # uv_coord = motion_cancel_mat @ uv_coord
 
-            # map_x : (H, W)
-            # map_y : (H, W)
-            # dst[i][j] = src[ map_y[i][j] ][ map_x[i][j] ]
             map_x[i][j] = uv_coord[0]
             map_y[i][j] = uv_coord[1]
             
-    return map_x, map_y
-
-def quaternion_rotation_matrix(Q):
-    q0, q1, q2, q3 = Q
-    R = np.array([[2 * (q0 * q0 + q1 * q1) - 1,  2 * (q1 * q2 - q0 * q3), 2 * (q1 * q3 + q0 * q2)],
-                            [2 * (q1 * q2 + q0 * q3), 2 * (q0 * q0 + q2 * q2) - 1, 2 * (q2 * q3 - q0 * q1)],
-                            [2 * (q1 * q3 - q0 * q2), 2 * (q2 * q3 + q0 * q1), 2 * (q0 * q0 + q3 * q3) - 1]])
-                            
-    return R
-
-def euler_from_quaternion(q):
-    x, y, z, w = q
-    
-    # t0 = +2.0 * (w * x + y * z)
-    # t1 = +1.0 - 2.0 * (x * x + y * y)
-    # roll_x = math.atan2(t0, t1)
-
-    # t2 = +2.0 * (w * y - z * x)
-    # t2 = +1.0 if t2 > +1.0 else t2
-    # t2 = -1.0 if t2 < -1.0 else t2
-    # pitch_y = math.asin(t2)
-
-    t3 = +2.0 * (w * z + x * y)
-    t4 = +1.0 - 2.0 * (y * y + z * z)
-    yaw = math.atan2(t3, t4)
-
-    matrix = [[math.cos(yaw), -math.sin(yaw)],
-             [math.sin(yaw), math.cos(yaw)]]
-    return matrix, yaw
-
-def get_bbox_corners(center, dimensions, wx_interval, wy_interval):
-    w, l, _ = dimensions
-    x, y, _ = center
-
-    l2, w2 = (l / 2)/wx_interval, (w / 2)/wy_interval
-
-    corner = np.array([
-        [x - l2, y - w2],  # Bottom-left
-        [x + l2, y - w2],  # Bottom-right
-        [x + l2, y + w2],  # Top-right
-        [x - l2, y + w2],  # Top-left
-    ])
-    return corner
-
-def rotate_bbox(center, corner, rotation_matrix):
-    diff = []
-    for corner_x, corner_y in corner:
-        dx = corner_x - center[0]
-        dy = corner_y - center[1]
-        diff.append([dx, dy])
-    diff = np.array(diff)
-    rotated_corner = np.dot(rotation_matrix, diff.T).T
-    rotated_corner = [[x+center[0], y+center[1]] for x, y in rotated_corner]
-    return rotated_corner
+    return map_x, map_y, output_height, output_width
 
 def main():
     split = 'train'
-    wx_min, wx_max, wx_interval, wy_min, wy_max, wy_interval = 7, 70, 0.05, -10, 10, 0.025
+    wx_min, wx_max, wx_interval, wy_min, wy_max, wy_interval = 7, 30, 0.05, -10, 10, 0.05
     bev_height, bev_width = int((wx_max-wx_min)/wx_interval), int((wy_max-wy_min)/wy_interval)
     dir_download = '/home/jingjingtapa/다운로드'
     dir_image = f'{dir_download}/leftImg8bit/{split}'
@@ -221,35 +126,33 @@ def main():
         print(box_dir)
 
         image, calibration, box = cv2.imread(image_dir), read_json(calibration_dir), read_json(box_dir)
+        
+        camera = Camera(fx=box["sensor"]["fx"],fy=box["sensor"]["fy"],
+                u0=box["sensor"]["u0"],v0=box["sensor"]["v0"],
+                sensor_T_ISO_8855=box["sensor"]["sensor_T_ISO_8855"])
+
+        box3d_annotation = Box3dImageTransform(camera=camera)
 
         extrinsic, intrinsic = load_camera_params(calibration)
 
         motion_cancel_mat = motion_cancel(box)
 
-        map_x, map_y = generate_direct_backward_mapping(wx_min, wx_max, wx_interval, wy_min, wy_max, wy_interval, extrinsic, intrinsic, motion_cancel_mat)
+        map_x, map_y, bev_height, bev_width = generate_direct_backward_mapping(wx_min, wx_max, wx_interval, wy_min, wy_max, wy_interval, extrinsic, intrinsic, motion_cancel_mat)
 
-        output_image = cv2.remap(image, map_x, map_y, cv2.INTER_NEAREST, borderMode=cv2.BORDER_CONSTANT)
+        bev_image = cv2.remap(image, map_x, map_y, cv2.INTER_NEAREST, borderMode=cv2.BORDER_CONSTANT)
 
-        # cv2.putText(output_image, image_dir, (10,10), cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255) )
+        obj = CsBbox3d()
 
-        for object in box["objects"]:
-            center, type, quarternion, dimension = object['3d']['center'], object['3d']['type'], object['3d']['rotation'], object['3d']['dimensions']
-            if center[0] >= wx_min and center[0] <= wx_max and center[1] >= wy_min and center[1] <= wy_max:
-                # 이미지 하단 정중앙을 (0,0)으로 좌표 변환
-                cx, cy = int(0.5*bev_width-center[1]/wy_interval), int(bev_height-center[0]/wx_interval)
-                rotation_mat, yaw = euler_from_quaternion(quarternion)
-                corner = get_bbox_corners(center= [cx,cy,1], dimensions =dimension, wx_interval= wx_interval, wy_interval= wy_interval)
-                rotated_corner = rotate_bbox([cx, cy], corner,rotation_mat)
+        for object in box['objects']:
+            obj.fromJsonText(object)
+            box3d_annotation.initialize_box_from_annotation(obj, coordinate_system=CRS_V)
+            box_vertices_C = box3d_annotation.get_vertices(coordinate_system=CRS_C)
+            for loc, pv_coord in box_vertices_C.items():
+                if loc in ['FRB', 'FLB', 'BLB', 'BRB']:           
+                    u, v = int(0.5*bev_width - (pv_coord[1])/wy_interval), int(bev_height - (pv_coord[0] - wx_min)/wx_interval)
+                    cv2.circle(bev_image, (u, v), 2, (255, 255, 0), -1, cv2.LINE_AA)
 
-                cv2.circle(output_image, (cx, cy), 5, (255, 255, 0), -1, cv2.LINE_AA)
-                for x,y in corner:
-                    cv2.circle(output_image, (int(x), int(y)), 5, (0, 255, 0), -1, cv2.LINE_AA)
-                
-                for x,y in rotated_corner:
-                    cv2.circle(output_image, (int(x), int(y)), 5, (255, 0, 0), -1, cv2.LINE_AA)
-                cv2.putText(output_image, f'{center}/{type}/yaw:{np.rad2deg(yaw)}', (cx-100, cy-10), cv2.FONT_HERSHEY_SIMPLEX,0.6,(255,255,0) )
-
-        cv2.imshow('BEV',output_image)
+        cv2.imshow('BEV',bev_image)
         cv2.imshow('original', image)
         key = cv2.waitKey(0)
         if key == ord('n') and cnt_image <= len(image_list) - 2:
